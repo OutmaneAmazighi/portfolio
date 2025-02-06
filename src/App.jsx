f26f20d90a337a20c2f0d991e32845ea60a67064
mohamedzas <mohamedzarro@outlook.com>
2025-02-06 13:49:13 +0100

    Removed Firebase test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Portfolio from './Portfolio'
-import FirebaseTest from './components/FirebaseTest.jsx';  // Updated extension
 
 function App() {
   return (
     <div className="App">
       <Portfolio />
-      <FirebaseTest />
     </div>
   )
 }
